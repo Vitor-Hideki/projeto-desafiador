0161a42ab19c21c22328ad9dd8388001e418d1e3
Vitor Hideki <nakata.vitor03@gmail.com>
2022-12-09 05:25:31 -0300

    projeto-desafiador-1.7

--- blogr-landing-page-main/assets/JavaScript/js.js
+++ blogr-landing-page-main/assets/JavaScript/js.js
@@ -5,22 +5,36 @@ document.addEventListener('click', (e) => {
 
    let currentSubmenu
 
+   let currentIcon
+
    if(isDropdownBtn){
     const item = e.target.closest('.li-p')
 
     currentSubmenu = item.querySelector('.submenu')
 
     currentSubmenu.classList.toggle('show')
+
+    currentIcon = item.querySelector('.icon')
+
+    currentIcon.classList.toggle('reverse')
    }
 
    document.querySelectorAll('.submenu.show').forEach((submenu) => {
     if(submenu == currentSubmenu) return;
 
     submenu.classList.remove('show');
+
+   });
+
+   document.querySelectorAll('.icon.reverse').forEach((reverse) => {
+    if(reverse == currentIcon) return;
+
+    reverse.classList.remove('reverse');
    });
 
 } );
 
+/*
 document.addEventListener('click', (e) => {
     const isDropdownBtn = e.target.classList.contains('bt1');
 
@@ -42,4 +56,4 @@ document.addEventListener('click', (e) => {
     reverse.classList.remove('reverse');
    });
 
-} );
+} );*/
